60b63824d3422e903f2d5a7236dbcbcb8e7f047f
Jagriti Maurya <jagritimaurya4156@gmail.com>
2026-08-02 19:18:02 +0530

    Time: 3 ms (79.95%), Space: 20.4 MB (75.39%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        
+        dict1={}
+
+        for i in range(n):
+            # rem is remender
+            rem=target-nums[i]
+            if rem in dict1:
+                return [dict1[rem]+1,i+1]
+            dict1[nums[i]]=i   
+        
+        
